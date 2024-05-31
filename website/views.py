358654a21fd98d283818cc51a590b60419f4a4b7
Eduardo Coelho <eceduardocoelho@gmail.com>
2024-05-31 16:50:11 -0300

    feat: add new record feature

--- website/views.py
+++ website/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import redirect, render
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
-from .forms import SignUpForm
+from .forms import SignUpForm, AddRecordForm
 from .models import Record
 
 def home(request): 
@@ -62,3 +62,18 @@ def delete_record(request, pk):
     else:
         messages.success(request, "You must be logged in...")
         return redirect('home')
+    
+def add_record(request):
+    form =AddRecordForm(request.POST or None)
+    if request.user.is_authenticated:
+        if request.method == "POST":
+            if form.is_valid():
+                add_record = form.save()
+                messages.success(request, "Record added...")
+                return redirect('home')
+        return render(request, 'add_record.html', {'form':form})
+    else:
+        messages.success(request, "You must be logged in...")
+        return redirect('home')
+            
+    
